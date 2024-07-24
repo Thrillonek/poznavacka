d8c235077c2a197e21d8545a5a241a19244f1b86
Thrillonek <83271284+Thrillonek@users.noreply.github.com>
2024-07-24 09:59:52 +0200

    test v2

--- src/App.jsx
+++ src/App.jsx
@@ -63,8 +63,8 @@ function App() {
 	return (
 		<div className='w-full h-svh flex flex-col justify-between items-center bg-gray-700'>
 			<div className='h-1/2 p-2'>
-				<img onLoad={() => setIndex(prev => {...prev, imgLoaded: true})} className='h-full max-h-full rounded' src={encodeURI('./assets/' + text).replaceAll("+","%2b")} />
-				<p className='text-2xl text-white text-center mt-2 font-semibold'>{show ? text.slice(0, -4).replace(/\d+/g, '') : index.imgLoaded == true ? index.number : "Loading..."}</p>
+				<img onLoad={() => setIndex({number: index number, imgLoaded: true})} className='h-full max-h-full rounded' src={encodeURI('./assets/' + text).replaceAll("+","%2b")} />
+				<p className='text-2xl text-white text-center mt-2 font-semibold'>{show ? text.slice(0, -4).replace(/\d+/g, '') : (index.imgLoaded == true ? index.number : "Loading...")}</p>
 			</div>
 			<div className='h-1/2 flex flex-col justify-around items-center'>
 				<p className='font-bold text-gray-300 text-xl'>
