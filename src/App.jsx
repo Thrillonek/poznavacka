ab190602d6159c631e937692cbdea0b049cefea0
Thrillonek <83271284+Thrillonek@users.noreply.github.com>
2024-07-25 21:51:50 +0200

    error handling

--- src/App.jsx
+++ src/App.jsx
@@ -42,15 +42,21 @@ function App() {
 	}, [min, max]);
 
 	function change() {
+  setError(null)
+  if(max <= min) return setError("Dolní hranice musí být nižší než ta horní")
+  if(!min || !max) return setError("Prosím vyplň obě hranice")
+  if(min < 1) return setError("Dolní hranice nemůže být nižší než 1")
+  if(max > files.lengtg) setError("Horní hranice nemůže být vyšší než "+files.length)
+
 		const rng = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 		let idx = rng(parseInt(min) - 1 || 0, parseInt(max) - 1 || files.length);
   let range = (max - min)
   let mid = Math.round(range / 2)
 		while (check.includes(idx)) {
-   if(range < 8) break
-   let adjustment = Math.round((max - min) /10)
-   if(mid >= idx)	 idx += adjustment
+   if(range < 8) break;
+   let adjustment = Math.round(range / 10)
+   if(mid > idx)	 idx += adjustment
    else idx -= adjustment
 		}
  
