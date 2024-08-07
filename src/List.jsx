14924ca9d75e718cc27b543c76ee35ce30361a3d
Thrillonek <jindrich.kraina@gmail.com>
2024-08-07 18:59:02 +0200

    files were not sorted in plant list

--- src/List.jsx
+++ src/List.jsx
@@ -9,20 +9,30 @@ export default function List() {
 
 	return (
 		<div className='flex flex-col bg-gray-700 min-h-[80svh]'>
-			{files.current?.map((file, idx) => {
-				let readableFile = file
-					.split('.')[0]
-					.replaceAll(/[0-9+_]/g, '')
-					.replace('-', ' - ');
-				return (
-					<div className='flex items-center border-gray-500 p-2 border-b h-20'>
-						<img src={('./assets/img/' + file).replace(' ', '%20').replace('+', '%2b')} alt='Obrázek rostliny' className='max-h-full' />
-						<span className='ml-5 font-bold text-gray-400 text-xl'>
-							{idx + 1}. {capitalize(readableFile)}
-						</span>
-					</div>
-				);
-			})}
+			{files.current
+				?.sort((a, b) => {
+					let ai = a.split('');
+					ai = ai.filter((n) => !isNaN(n));
+					ai = ai.join('');
+					let bi = b.split('');
+					bi = bi.filter((n) => !isNaN(n));
+					bi = bi.join('');
+					return parseInt(ai) - parseInt(bi);
+				})
+				.map((file, idx) => {
+					let readableFile = file
+						.split('.')[0]
+						.replaceAll(/[0-9+_]/g, '')
+						.replace('-', ' - ');
+					return (
+						<div className='flex items-center border-gray-500 p-2 border-b h-20'>
+							<img src={('./assets/img/' + file).replace(' ', '%20').replace('+', '%2b')} alt='Obrázek rostliny' className='max-h-full' />
+							<span className='ml-5 font-bold text-gray-400 text-xl'>
+								{idx + 1}. {capitalize(readableFile)}
+							</span>
+						</div>
+					);
+				})}
 		</div>
 	);
 }
