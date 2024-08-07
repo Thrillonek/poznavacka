9b0b066b3cfcf191337eb0ef6ebd06b9bcc710cc
Thrillonek <jindrich.kraina@gmail.com>
2024-08-07 20:07:21 +0200

    filter added in plant list page

--- src/List.jsx
+++ src/List.jsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from 'react';
 
 export default function List() {
+	const [filter, setFilter] = useState();
+
 	const files = useRef(__FILES__);
 
 	function capitalize(str) {
@@ -8,9 +10,17 @@ export default function List() {
 	}
 
 	return (
-		<div className='flex flex-col bg-gray-700 min-h-[80svh]'>
+		<div className='flex flex-col bg-gray-700 min-h-svh'>
+			<div className='flex items-center w-full'>
+				<label className='mx-3 text-gray-500' htmlFor='imgFilter'>
+					<i className='mr-1 fa-filter fa-solid' />
+					Filtr
+				</label>
+				<input onChange={(e) => setFilter(e.target.value)} value={filter} id='imgFilter' type='text' className='flex-grow bg-gray-600 p-1 text-gray-200 caret-gray-400 outline-none' />
+			</div>
 			{files.current
 				?.sort((a, b) => parseInt(a.replaceAll(/\D/g, '')) - parseInt(b.replaceAll(/\D/g, '')))
+				.filter((f) => (filter ? f.includes(filter) : f))
 				.map((file, idx) => {
 					let readableFile = file
 						.split('.')[0]
@@ -20,7 +30,7 @@ export default function List() {
 						<div className='flex items-center border-gray-500 p-2 border-b h-20'>
 							<img src={('./assets/img/' + file).replace(' ', '%20').replace('+', '%2b')} alt='Obrázek rostliny' className='max-h-full' />
 							<span className='ml-5 font-bold text-gray-400 text-xl'>
-								{idx + 1}. {capitalize(readableFile)}
+								{file.replaceAll(/[^0-9]/g, '')}. {capitalize(readableFile)}
 							</span>
 						</div>
 					);
