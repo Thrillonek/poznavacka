ae3e6f4b0402efed8468dbdfe4c9073052c4cbd0
Thrillonek <jindrich.kraina@gmail.com>
2024-09-12 16:14:01 +0200

    fixed plant names not appearing after clearing filter

--- src/List.jsx
+++ src/List.jsx
@@ -7,7 +7,6 @@ export default function List() {
 	const files = useRef(__FILES__);
 
 	useEffect(() => {
-  if(chosenFile) return
 		const observer = new IntersectionObserver((entries) => {
 			entries.forEach(
 				(entry) => {
@@ -23,7 +22,13 @@ export default function List() {
 		document.querySelectorAll('.plant-list-item')?.forEach((el) => {
 			observer.observe(el);
 		});
-	}, [chosenFile]);
+
+		return () => {
+			document.querySelectorAll('.plant-list-item')?.forEach((el) => {
+				observer.unobserve(el);
+			});
+		};
+	}, [filter]);
 
 	function capitalize(str) {
 		return str.charAt(0).toUpperCase() + str.slice(1);
@@ -49,39 +54,35 @@ export default function List() {
 					</button>
 				</div>
 			)}
-				{!chosenFile && (
-     <>
-					<div className='flex items-center w-full'>
-						<label className='bg-gray-700 px-3 py-1 text-gray-400' htmlFor='imgFilter'>
-							<i className='mr-1 fa-filter fa-solid' />
-							Filtr
-						</label>
-						<input onChange={(e) => setFilter(e.target.value)} value={filter} id='imgFilter' type='text' className='flex-grow bg-gray-600 p-1 text-gray-200 caret-gray-400 outline-none' />
-					</div>
-				
-				<div className='overflow-y-scroll'>
-					{files.current
-						?.sort((a, b) => parseInt(a.replaceAll(/\D/g, '')) - parseInt(b.replaceAll(/\D/g, '')))
-						.map((file, idx) => {
-							let readableFile = file
-								.split('.')[0]
-								.replaceAll(/[0-9+_]/g, '')
-								.replace('-', ' - ');
+			<div className='flex items-center w-full'>
+				<label className='bg-gray-700 px-3 py-1 text-gray-400' htmlFor='imgFilter'>
+					<i className='mr-1 fa-filter fa-solid' />
+					Filtr
+				</label>
+				<input onChange={(e) => setFilter(e.target.value)} value={filter} id='imgFilter' type='text' className='flex-grow bg-gray-600 p-1 text-gray-200 caret-gray-400 outline-none' />
+			</div>
 
-							if (!filter || (/\d/.test(filter) && (idx + 1).toString().startsWith(filter)) || (isNaN(filter) && file.includes(filter.toLowerCase()))) {
-								return (
-									<div key={idx} onClick={(e) => setChosenFile(file)} className='flex items-center border-gray-500 p-2 border-b h-20'>
-										<img src={('./assets/img/' + file).replace(' ', '%20').replace('+', '%2b')} alt='Obrázek rostliny' className='max-h-full' />
-										<span className='opacity-0 ml-5 font-bold text-gray-400 text-xl transition-all -translate-x-32 duration-[400ms] plant-list-item'>
-											{idx + 1}. {capitalize(readableFile)}
-										</span>
-									</div>
-								);
-							}
-						})}
-				</div>
-</>
-    )}
+			<div className='overflow-y-scroll'>
+				{files.current
+					?.sort((a, b) => parseInt(a.replaceAll(/\D/g, '')) - parseInt(b.replaceAll(/\D/g, '')))
+					.map((file, idx) => {
+						let readableFile = file
+							.split('.')[0]
+							.replaceAll(/[0-9+_]/g, '')
+							.replace('-', ' - ');
+
+						if (!filter || (/\d/.test(filter) && (idx + 1).toString().startsWith(filter)) || (isNaN(filter) && file.includes(filter.toLowerCase()))) {
+							return (
+								<div key={idx} onClick={(e) => setChosenFile(file)} className='flex items-center border-gray-500 p-2 border-b h-20'>
+									<img src={('./assets/img/' + file).replace(' ', '%20').replace('+', '%2b')} alt='Obrázek rostliny' className='max-h-full' />
+									<span className='opacity-0 ml-5 font-bold text-gray-400 text-xl transition-all -translate-x-32 duration-[400ms] plant-list-item'>
+										{idx + 1}. {capitalize(readableFile)}
+									</span>
+								</div>
+							);
+						}
+					})}
+			</div>
 		</div>
 	);
 }
