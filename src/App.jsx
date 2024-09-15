37092a2639bdb1e6594a63faee51717b596b5c4d
Thrillonek <jindrich.kraina@gmail.com>
2024-09-15 15:44:12 +0200

    switched default mode to list

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import List from './List.jsx';
 import Quiz from './Quiz.jsx';
 
 export default function App() {
-	const [mode, setMode] = useState('quiz');
+	const [mode, setMode] = useState('learning');
 
 	return (
 		<div className='flex flex-col w-screen h-dvh overflow-x-hidden'>
