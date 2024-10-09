88bb65d8247c4885dcec970346eb05d7740b09f6
Thrillonek <jindrich.kraina@gmail.com>
2024-10-09 17:44:35 +0200

    Added reset button to test

--- src/Test.jsx
+++ src/Test.jsx
@@ -8,10 +8,17 @@ function Test() {
 	const arr = useRef([]);
 
 	useEffect(() => {
+		if (arr.current.length > 0) return;
 		for (let i = 1; i <= 20; i++) {
 			arr.current.push(i * 7 - 1);
 		}
-	}, []);
+	}, [arr.current]);
+
+	function reset() {
+		arr.current = [];
+		setCount(0);
+		setPlant();
+	}
 
 	function changePlant() {
 		const chosen = arr.current[Math.floor(Math.random() * arr.current.length)];
@@ -25,7 +32,7 @@ function Test() {
 			<h1 className='font-bold text-3xl text-center text-white'>Krytosemenné rostliny poznávačka</h1>
 			{plant ? (
 				<div className='flex flex-col items-center h-1/2'>
-					<h2 className='py-2 font-bold text-white text-xl'>{count}</h2>
+					<h2 className='mb-4 py-2 font-bold text-2xl text-white'>{count}</h2>
 					<img src={('./assets/rostliny/' + plant).replace(' ', '%20').replace('+', '%2b')} className='h-[90%]' alt='obrázek rostliny' />
 				</div>
 			) : (
@@ -34,9 +41,14 @@ function Test() {
 				</h1>
 			)}
 			{plant && (
-				<button onClick={changePlant} className='flex justify-center items-center bg-blue-500 p-3 rounded-xl w-1/2 font-bold text-lg text-white'>
-					Další
-				</button>
+				<div className='flex flex-col items-center w-full'>
+					<button onClick={changePlant} className='flex justify-center items-center bg-blue-500 p-3 rounded-xl w-1/2 font-bold text-lg text-white'>
+						Další
+					</button>
+					<p onClick={reset} className='mt-4 text-white hover:text-red-400 cursor-pointer'>
+						Resetovat
+					</p>
+				</div>
 			)}
 		</div>
 	);
