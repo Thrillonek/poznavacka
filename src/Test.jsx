248d90e7b6d051f08168ad572b07736a0b68e534
Thrillonek <jindrich.kraina@gmail.com>
2024-10-09 17:37:26 +0200

    Design changes on corrupted rng

--- src/Test.jsx
+++ src/Test.jsx
@@ -8,11 +8,9 @@ function Test() {
 	const arr = useRef([]);
 
 	useEffect(() => {
-		for (let i = 0; i < 20; i++) {
-			arr.current.push(i * 7);
+		for (let i = 1; i <= 20; i++) {
+			arr.current.push(i * 7 - 1);
 		}
-		changePlant();
-		setCount(1);
 	}, []);
 
 	function changePlant() {
@@ -25,13 +23,21 @@ function Test() {
 	return (
 		<div className='flex flex-col justify-between items-center bg-gray-700 p-5 w-full h-full'>
 			<h1 className='font-bold text-3xl text-center text-white'>Krytosemenné rostliny poznávačka</h1>
-			<div className='flex flex-col items-center h-1/2'>
-				<h2 className='py-2 font-bold text-white text-xl'>{count}</h2>
-				<img src={('./assets/rostliny/' + plant).replace(' ', '%20').replace('+', '%2b')} className='h-[90%]' alt='obrázek rostliny' />
-			</div>
-			<button onClick={changePlant} className='flex justify-center items-center bg-blue-500 p-3 rounded-xl w-1/2 font-bold text-white'>
-				Další
-			</button>
+			{plant ? (
+				<div className='flex flex-col items-center h-1/2'>
+					<h2 className='py-2 font-bold text-white text-xl'>{count}</h2>
+					<img src={('./assets/rostliny/' + plant).replace(' ', '%20').replace('+', '%2b')} className='h-[90%]' alt='obrázek rostliny' />
+				</div>
+			) : (
+				<h1 onClick={changePlant} className='top-1/2 absolute p-8 font-bold text-5xl text-white tracking-widest cursor-pointer'>
+					ZAČÍT
+				</h1>
+			)}
+			{plant && (
+				<button onClick={changePlant} className='flex justify-center items-center bg-blue-500 p-3 rounded-xl w-1/2 font-bold text-lg text-white'>
+					Další
+				</button>
+			)}
 		</div>
 	);
 }
