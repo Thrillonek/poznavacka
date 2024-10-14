f66b2d68d482584bd56a54a55d5bd97a2299b5dd
Thrillonek <jindrich.kraina@gmail.com>
2024-10-14 19:16:33 +0200

    Cookies now load optionally through backend

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Test from './Test.jsx';
 export default function App() {
 	const [poznavacka, setPoznavacka] = useState();
 	const [showingContent, setShowingContent] = useState();
+	const [loaded, setLoaded] = useState(false);
 
 	useEffect(() => {
 		const cookie = Cookies.get('poznavacka');
@@ -17,12 +18,33 @@ export default function App() {
 		} else {
 			setPoznavacka('rostliny');
 		}
+		axios
+			.get('/api/index')
+			.then((res) => {
+				if (loaded == 'block') return;
+				setLoaded(true);
+				if (res.data.session) {
+					setPoznavacka(res.data.session);
+					setShowingContent(true);
+				} else {
+					setPoznavacka('rostliny');
+				}
+			})
+			.catch((err) => {
+				setPoznavacka('rostliny');
+				console.error(err.response.data.error);
+			});
 	}, []);
 
 	useEffect(() => {
 		let data = poznavacka;
 		if (!showingContent) data = '';
 		Cookies.set('poznavacka', data);
+		axios
+			.post('/api/index', {
+				poznavacka: data,
+			})
+			.catch((err) => console.error(err.response.data.message));
 	}, [poznavacka, showingContent]);
 
 	function showContent(pozn) {
@@ -33,13 +55,13 @@ export default function App() {
 	if (!poznavacka) return;
 	return (
 		<Router>
-			<div className='relative flex flex-col items-center bg-gray-700 w-screen h-dvh overflow-y-hidden'>
+			<div onClick={(e) => setLoaded('block')} className='relative flex flex-col items-center bg-gray-700 w-screen h-dvh overflow-y-hidden'>
 				<Routes>
 					<Route
 						path=''
 						element={
 							<>
-								<div className={'absolute h-full w-full translate-y-full transition-transform duration-500 flex flex-col ' + (showingContent && '!translate-y-0')}>
+								<div className={'absolute h-full z-10 w-full translate-y-full transition-transform duration-500 flex flex-col ' + (showingContent && '!translate-y-0')}>
 									<div onClick={(e) => setShowingContent(false)} className='border-gray-500 bg-[rgb(45,55,71)] p-1 border-b w-full text-gray-400'>
 										<span className='ml-2 cursor-pointer'>
 											<i className='fa-arrow-left mr-1 fa-solid'></i>Zpět na výběr
@@ -49,8 +71,11 @@ export default function App() {
 										<Home poznavacka={poznavacka} />
 									</div>
 								</div>
-								<div className='flex flex-col px-10 w-full'>
-									<h1 className='mt-10 mb-5 font-bold text-3xl text-gray-400'>Vyber poznávačku:</h1>
+								<div className='relative z-0 flex flex-col px-10 w-full'>
+									<div className={'top-0 transition-transform absolute bg-gray-700 shadow-[0_0_30px_0_rgb(0,0,0,0.5)] px-4 py-2 rounded-b-xl font-bold text-center text-gray-400 self-center ' + (loaded && '-translate-y-[150%]')}>
+										Načítám cookies... <span className='block font-normal text-[.7rem]'>Jakoukoli akcí toto zastavíte</span>
+									</div>
+									<h1 className='mt-10 mb-5 font-bold text-3xl text-gray-200'>Vyber poznávačku:</h1>
 									<button onClick={(e) => showContent('rostliny')} className='bg-gray-600 m-5 py-10 rounded-xl w-[90%] font-bold text-5xl text-gray-300 self-center'>
 										Rostliny
 									</button>
