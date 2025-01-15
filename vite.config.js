942c2cc7299c7984c9ed78f950e8806b119cc075
Thrillonek <jindrich.kraina@gmail.com>
2025-01-15 17:55:53 +0100

    help

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite';
 
 let path = './public/assets/rostliny';
 const rostliny = fs.readdirSync(path).sort((a, b) => parseInt(a.replace(/\D/g, '')) - parseInt(b.replaceAll(/\D/g, '')));
+const rostliny = fs.readdirSync(path).sort((a, b) => parseInt(a.replace(/\D/g, '')) - parseInt(b.replaceAll(/\D/g, '')));
+
 path = './public/assets/houby';
 const houby = fs.readdirSync(path).sort((a, b) => {
 	if (/\d/.test(a.slice(0, 3)) && /\d/.test(b.slice(0, 3))) {
@@ -27,7 +29,7 @@ const prvousti = fs
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: './',
+	// base: './',
 	plugins: [react()],
 	define: {
 		__ROSTLINY__: JSON.stringify(rostliny),
