cb93056da2efb86da705c36012c75a64db76d43c
Thrillonek <jindrich.kraina@gmail.com>
2025-04-01 19:23:48 +0200

    New poznavacka

--- index.js
+++ index.js
@@ -14,25 +14,38 @@ app.use(express.json());
 // let files = fs.readdirSync(path);
 // let plus = 0;
 // let arr = [];
+// let count = 0;
+// let currentTitle = '';
 // for (const file of files.sort((a, b) => a.replaceAll(/\D/g, '') - b.replaceAll(/\D/g, ''))) {
 // 	let content = fs.readFileSync(path + '/' + file);
 // 	let $ = load(content, { xml: true });
 // 	let num = file.replaceAll(/\D/g, '');
+// 	// count += $('p\\:pic').length;
+// 	if ($('p\\:pic').length == 0) continue;
 // 	$(`a\\:t`).each((idx, item) => {
 // 		item.children.forEach((el) => {
 // 			let { data } = el;
-// 			if (idx > 0) plus += 1;
-// 			arr.push(data);
+// 			currentTitle = data;
+// if (idx > 0) plus += 1;
+// for (let i = 0; i < $('p\\:pic').length; i++) {
+// 	arr.push(data);
+// }
 // 		});
 // 	});
-// }
-// fs.writeFileSync('./public/text.txt', `${arr}`, 'utf8');
 
-let content = fs.readFileSync('./public/text.txt', 'utf-8');
+// 	for (let i = 0; i < $('p\\:pic').length; i++) {
+// 		arr.push(currentTitle);
+// 	}
+// }
+// fs.writeFileSync('./public/hmyz.txt', `${arr.join(',\n')}`, 'utf8');
+// console.log(count);
+
+//WRITING SYSTEM
+let content = fs.readFileSync('./public/hmyz.txt', 'utf-8');
 let arr = content.split(',\n');
 
-let first = [];
-let used = [];
+// let first = [];
+// let used = [];
 
 // for (const i of arr) {
 // 	if (first.includes(i)) {
@@ -44,20 +57,23 @@ let used = [];
 
 // console.log(used);
 
-// let files = fs.readdirSync('./public/assets/media').sort((a, b) => a.replaceAll(/\D/g, '') - b.replaceAll(/\D/g, ''));
+let files = fs.readdirSync('./media').sort((a, b) => a.replaceAll(/\D/g, '') - b.replaceAll(/\D/g, ''));
 // console.log(files.length);
 // let newFiles = files.map((f) => f.replaceAll(/\d/g, ''));
-// for (const idx in files) {
-// 	fs.rename('./public/assets/media/' + files[idx], './public/assets/media/' + arr[idx] + '.jpg', (err) => err && console.log(err));
-// }
-
-let myfiles = fs.readFileSync('./public/moje.txt', 'utf-8').split('\n');
-let files = fs.readFileSync('./public/test.txt', 'utf-8').split('\n');
-
-for (const file of files) {
-	if (myfiles.includes(file)) continue;
-	console.log(file);
+for (const idx in files) {
+	fs.rename('./media/' + files[idx], './media/' + arr[idx] + idx + '.' + files[idx].split('.').at(-1), (err) => err && console.log(err));
+	// if (idx < 20) {
+	// 	console.log('./media/' + arr[idx] + '.' + files[idx].split('.').at(-1));
+	// }
 }
+
+// let myfiles = fs.readFileSync('./public/moje.txt', 'utf-8').split('\n');
+// let files = fs.readFileSync('./public/test.txt', 'utf-8').split('\n');
+
+// for (const file of files) {
+// 	if (myfiles.includes(file)) continue;
+// 	console.log(file);
+// }
 
 app.get('/', (req, res) => {
 	res.sendFile('./dist/index.html');
