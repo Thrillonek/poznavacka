b2d5c5b62a9e3cc82f03b1684b77f4ed7e62763c
Thrillonek <jindrich.kraina@gmail.com>
2025-10-04 10:29:29 +0200

    removeDuplicate function only runs when needed

--- src/hooks/useUpdatePoznavacka.js
+++ src/hooks/useUpdatePoznavacka.js
@@ -1,16 +1,24 @@
 import { usePoznavackaStore, useSettingsStore } from '@/data';
 import { removeDuplicateFileNames } from '@/utils/removeDuplicateFileNames';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export function useUpdatePoznavacka() {
 	const basePoznavacka = usePoznavackaStore((state) => state.basePoznavacka);
 	const updatePoznavacka = usePoznavackaStore((state) => state.updatePoznavacka);
 	const settings = useSettingsStore((state) => state.settings);
 
+	const poznavackaWithoutDuplicates = useRef();
+
+	useEffect(() => {
+		poznavackaWithoutDuplicates.current = null;
+	}, [basePoznavacka]);
+
 	useEffect(() => {
 		if (settings.removeDuplicates) {
-			const updatedPoznavacka = removeDuplicateFileNames(basePoznavacka);
-			updatePoznavacka(updatedPoznavacka);
+			if (poznavackaWithoutDuplicates.current == null) {
+				poznavackaWithoutDuplicates.current = removeDuplicateFileNames(basePoznavacka);
+			}
+			updatePoznavacka(poznavackaWithoutDuplicates.current);
 		} else {
 			updatePoznavacka(basePoznavacka);
 		}
