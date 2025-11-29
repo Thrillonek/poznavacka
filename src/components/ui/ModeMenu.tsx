d732b4eabab955b25bf632517122992fcdc87bb7
Thrillonek <jindrich.kraina@gmail.com>
2025-11-29 15:04:47 +0100

    You now cannot interact with mode menu when you have not chosen a set

--- src/components/ui/ModeMenu.tsx
+++ src/components/ui/ModeMenu.tsx
@@ -1,19 +1,25 @@
 import { Icon } from '@iconify/react';
+import { useMemo } from 'react';
 import 'src/assets/_ModeMenu.scss';
-import { useModeStore } from 'src/data/stores';
+import { useModeStore, usePoznavackaStore } from 'src/data/stores';
+import { getFiles } from 'src/utils';
 
 function ModeMenu() {
 	const { mode, setMode } = useModeStore((store) => store);
 
+	const poznavacka = usePoznavackaStore((store) => store.poznavacka!);
+
+	const files = useMemo(() => getFiles(), [poznavacka]);
+
 	return (
-		<div className='shadow-base mode-menu'>
+		<div className={'shadow-base mode-menu' + (files.length == 0 ? ' disabled' : '')}>
 			<button onClick={() => setMode('quiz')} className={mode == 'quiz' ? 'active' : ''}>
 				<Icon icon='mdi:brain' />
 			</button>
 			<button onClick={() => setMode('list')} className={mode == 'list' ? 'active' : ''}>
 				<Icon icon='mdi:format-list-bulleted-square' />
 			</button>
-			<button onClick={() => setMode('settings')} className={mode == 'settings' ? 'active' : ''}>
+			<button data-settings onClick={() => setMode('settings')} className={mode == 'settings' ? 'active' : ''}>
 				<Icon icon='mdi:gear' />
 			</button>
 		</div>
