1b1dc398efd35adbcc9479c81e55c5d0d29e7a7e
Thrillonek <jindrich.kraina@gmail.com>
2025-11-30 08:06:06 +0100

    Previous fix didnt work, this one does

--- src/components/ui/ModeMenu.tsx
+++ src/components/ui/ModeMenu.tsx
@@ -1,5 +1,5 @@
 import { Icon } from '@iconify/react';
-import { useCallback } from 'react';
+import { useMemo } from 'react';
 import 'src/assets/_ModeMenu.scss';
 import { useModeStore, usePoznavackaStore } from 'src/data/stores';
 import { getFiles } from 'src/utils';
@@ -9,12 +9,10 @@ function ModeMenu() {
 
 	const poznavacka = usePoznavackaStore((store) => store.poznavacka!);
 
-	const files = useCallback(() => {
-		if (poznavacka) getFiles();
-	}, [poznavacka]);
+	const files = useMemo(() => poznavacka && getFiles(), [poznavacka]);
 
 	return (
-		<div className={'shadow-base mode-menu' + (files?.length == 0 ? ' disabled' : '')}>
+		<div className={'shadow-base mode-menu' + (!files || files.length == 0 ? ' disabled' : '')}>
 			<button onClick={() => setMode('quiz')} className={mode == 'quiz' ? 'active' : ''}>
 				<Icon icon='mdi:brain' />
 			</button>
