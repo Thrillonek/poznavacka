ee96dd6eb2f320a09eee1572243ef6187df6bfef
Thrillonek <jindrich.kraina@gmail.com>
2025-11-30 11:36:11 +0100

    Quiz controls new design

--- src/features/quiz/components/QuizControlPanel.tsx
+++ src/features/quiz/components/QuizControlPanel.tsx
@@ -12,24 +12,26 @@ function QuizControlPanel() {
 	let isPreviousAvailable = previousFiles.length > 1 && previousFiles[0] != fileIndex;
 
 	return (
-		<div className='flex max-md:flex-col max-md:items-center gap-4 md:gap-8 row-span-3 mx-auto mt-auto'>
-			<div className='place-items-center grid grid-flow-col bg-neutral-700 rounded-xl w-fit overflow-hidden'>
-				<button data-text={isPreviousAvailable || !settings.quiz.random ? 'Předchozí' : 'Zpět'} onClick={showPreviousFile} className={'control-btn ' + (!(previousFiles.length > 1) ? 'control-btn-disabled' : '')}>
-					<Icon icon='tabler:reload' className={isPreviousAvailable || !settings.quiz.random ? 'mb-5 -scale-x-100' : ''} />
-				</button>
-			</div>
-			<div className='place-items-center grid grid-flow-col bg-neutral-700 rounded-xl h-18 overflow-hidden'>
-				<button data-text={isFileNameRevealed ? 'Skryt' : 'Zobrazit'} className='control-btn' onClick={() => toggleFileNameRevealed()}>
-					{isFileNameRevealed ? <Icon icon='mdi:eye-off' /> : <Icon icon='mdi:eye' />}
-				</button>
-				<div className='bg-neutral-600 w-px h-2/3'></div>
-				<button data-text={settings.quiz.random ? 'Generovat' : 'Další'} onClick={() => changeImage()} className='control-btn'>
-					{settings.quiz.random ? <Icon icon='ion:dice' /> : <Icon icon='material-symbols:chevron-right-rounded' className='text-[1.5em]' />}
-				</button>
-				<div className='bg-neutral-600 w-px h-2/3'></div>
-				<button data-text='Naučeno' onClick={addFileToCompleted} className='control-btn'>
-					<Icon icon='material-symbols:check-circle-rounded' />
-				</button>
+		<div className='center-content'>
+			<div className='quiz-control-panel-container'>
+				<div className='quiz-control-panel'>
+					<button onClick={showPreviousFile} className={'control-button ' + (!(previousFiles.length > 1) ? 'disabled' : '')}>
+						<Icon icon='mdi:undo' />
+						<span>{isPreviousAvailable || !settings.quiz.random ? 'Předchozí' : 'Zpět'}</span>
+					</button>
+					<button onClick={addFileToCompleted} className='control-button'>
+						<Icon icon='mdi:checkbox-marked-circle-outline' />
+						<span>Splnit</span>
+					</button>
+					<button className='control-button' onClick={() => toggleFileNameRevealed()}>
+						<Icon icon={'mdi:eye' + (isFileNameRevealed ? '-off' : '')} />
+						<span>{isFileNameRevealed ? 'Skrýt název' : 'Ukázat název'}</span>
+					</button>
+					<button onClick={() => changeImage()} className='control-button'>
+						<Icon icon={settings.quiz.random ? 'mdi:image-refresh' : 'mdi:image-move'} />
+						<span>{settings.quiz.random ? 'Vygenerovat nový obrázek' : 'Další obrázek'}</span>
+					</button>
+				</div>
 			</div>
 		</div>
 	);
