16c1d81e13875a1be448200f289c09b3d61081ee
Thrillonek <jindrich.kraina@gmail.com>
2025-12-06 11:23:23 +0100

    Delete button done

--- src/features/settings/components/pages/GeneralSettings.tsx
+++ src/features/settings/components/pages/GeneralSettings.tsx
@@ -1,15 +1,16 @@
-import { useSettingsStore } from 'src/data';
+import { useCompletedFilesStore, useSettingsStore } from 'src/data';
 import DeleteButton from '../ui/DeleteButton';
 import SwitchInput from '../ui/SwitchInput';
 
 function GeneralSettings() {
 	const settings = useSettingsStore((store) => store.settings);
 	const updateCoreSettings = useSettingsStore((store) => store.updateCoreSettings);
+	const clearCompletedFiles = useCompletedFilesStore((store) => store.clearCompletedFiles);
 
 	return (
 		<>
 			<SwitchInput title='Odstranit duplikované obrázky' description='Zajistí, že každý název bude v poznávačce jen jednou' active={settings.removeDuplicates} toggle={() => updateCoreSettings('removeDuplicates', !settings.removeDuplicates)} />
-			<DeleteButton title='Resetovat naučené obrázky' text='Tímto u všech obrázků ve všech poznávačkách vymažeš označení "naučené" (začnou se ti objevovat ve kvízu).' />
+			<DeleteButton title='Resetovat naučené obrázky' text='Tímto u všech obrázků ve všech poznávačkách vymažeš označení "naučené" (začnou se ti objevovat ve kvízu).' confirmText='Resetovat' onConfirm={() => clearCompletedFiles()} />
 		</>
 	);
 }
--- src/features/settings/components/ui/DeleteButton.tsx
+++ src/features/settings/components/ui/DeleteButton.tsx
@@ -4,10 +4,17 @@ import classes from '../../assets/ui/_DeleteButton.module.scss';
 type DeleteButtonProps = {
 	title: string;
 	text: string;
+	confirmText: string;
+	onConfirm: () => void;
 };
 
-function DeleteButton({ title, text }: DeleteButtonProps) {
+function DeleteButton({ title, text, confirmText, onConfirm }: DeleteButtonProps) {
 	const [isModalOpen, setIsModalOpen] = useState(false);
+
+	function confirmModal() {
+		setIsModalOpen(false);
+		onConfirm();
+	}
 
 	return (
 		<>
@@ -16,16 +23,18 @@ function DeleteButton({ title, text }: DeleteButtonProps) {
 			</button>
 			<div onClick={() => setIsModalOpen(false)} data-open={isModalOpen} className={classes.modal}>
 				<div onClick={(e) => e.stopPropagation()}>
-					<h2>Opravdu chceš {title.toLowerCase()}?</h2>
-					<p>{text}</p>
-					{/* <div className='flex gap-x-4 mt-8'>
-						<button className={classes['btn-danger']} onClick={() => setIsModalOpen(false)}>
-							Ano
+					<div>
+						<h2>Opravdu chceš {title.toLowerCase()}?</h2>
+						<p>{text}</p>
+					</div>
+					<div className='flex gap-x-4'>
+						<button className={classes['modal-btn-confirm']} onClick={() => confirmModal()}>
+							{confirmText}
 						</button>
-						<button className={classes['btn-danger']} onClick={() => setIsModalOpen(false)}>
-							Ne
+						<button className={classes['modal-btn-close']} onClick={() => setIsModalOpen(false)}>
+							Zrušit
 						</button>
-					</div> */}
+					</div>
 				</div>
 			</div>
 		</>
