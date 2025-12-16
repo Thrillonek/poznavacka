af17e59acdcdd5a3344701b3e27db15c432211ad
Thrillonek <jindrich.kraina@gmail.com>
2025-12-16 21:49:21 +0100

    Removed console log from provider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,16 +1,12 @@
 import type { PropsWithChildren } from 'react';
 import 'src/assets/_global.scss';
-import { useAddEventListener, useInitiateDragEvent, useInitiateSwipeEvent } from 'src/hooks';
+import { useInitiateDragEvent, useInitiateSwipeEvent } from 'src/hooks';
 import { useUpdatePoznavacka } from 'src/hooks/useUpdatePoznavacka';
 
 export function AppProvider({ children }: PropsWithChildren) {
 	useUpdatePoznavacka();
 	useInitiateSwipeEvent();
 	useInitiateDragEvent();
-
-	useAddEventListener('custom:drag', (e) => {
-		console.log(e.detail.deltaX, e.detail.deltaY);
-	});
 
 	return <>{children}</>;
 }
