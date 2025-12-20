42620295c572a15e687271b447b05d4f59e1f082
Thrillonek <jindrich.kraina@gmail.com>
2025-12-20 20:09:52 +0100

    Testing, made the quiz swipe proceed distance smaller

--- src/features/quiz/data/constants.ts
+++ src/features/quiz/data/constants.ts
@@ -1,2 +1,2 @@
 const mainContentWidth = document.querySelector('.main-content')?.getBoundingClientRect().width || window.innerWidth;
-export const quizDragOffsetLimit = mainContentWidth / 5;
+export const quizDragOffsetLimit = mainContentWidth / 7.5;
