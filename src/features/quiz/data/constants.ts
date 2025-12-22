fbe779aa6841f5cd147d6830c588e511e9ed4bbf
Thrillonek <jindrich.kraina@gmail.com>
2025-12-22 11:49:29 +0100

    Made the quiz swiping distance a lot shorter

--- src/features/quiz/data/constants.ts
+++ src/features/quiz/data/constants.ts
@@ -1,2 +1,2 @@
-const mainContentWidth = document.querySelector('.main-content')?.getBoundingClientRect().width || window.innerWidth;
-export const quizDragOffsetLimit = mainContentWidth / 7.5;
+// const mainContentWidth = document.querySelector('.main-content')?.getBoundingClientRect().width || window.innerWidth;
+export const quizDragOffsetLimit = 30;
