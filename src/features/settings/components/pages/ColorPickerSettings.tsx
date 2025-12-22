d363874b4ef1a5630cc0fe40b37eaf55f81c6df9
Thrillonek <jindrich.kraina@gmail.com>
2025-12-22 16:42:34 +0100

    You can now select theme (only light now

--- src/features/settings/components/pages/ColorPickerSettings.tsx
+++ src/features/settings/components/pages/ColorPickerSettings.tsx
@@ -1,16 +1,19 @@
 import SelectionInput from 'src/components/form/SelectionInput';
 import { useSettingsStore } from 'src/data';
+import { useChangeTheme } from '../../hooks/useChangeTheme';
 
 function ColorPickerSettings() {
 	const settings = useSettingsStore((store) => store.settings);
 	const updateSettings = useSettingsStore((store) => store.updateSettings);
 
+	useChangeTheme(settings.colorPicker.preset);
+
 	return (
 		<>
 			<div className='settings-section'>
 				<h3>Motivy</h3>
-				<SelectionInput title='Tmavý' active={settings.colorPicker.preset === 'dark'} type='radio' onSelect={() => updateSettings('list', 'showFiles', 'all')} />
-				<SelectionInput title='Světlý' active={settings.colorPicker.preset === 'light'} type='radio' onSelect={() => updateSettings('list', 'showFiles', 'uncompleted')} />
+				<SelectionInput title='Tmavý' active={settings.colorPicker.preset === 'dark'} type='radio' onSelect={() => updateSettings('colorPicker', 'preset', 'dark')} />
+				<SelectionInput title='Světlý' active={settings.colorPicker.preset === 'light'} type='radio' onSelect={() => updateSettings('colorPicker', 'preset', 'light')} />
 			</div>
 		</>
 	);
---
+++ src/features/settings/hooks/useChangeTheme.ts
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+
+export function useChangeTheme(theme: string) {
+	useEffect(() => {
+		if (theme == 'dark') {
+			setColorVariables('dark', {
+				bgDark: 'oklch(0.1822 0 0)',
+				bgNormal: 'oklch(0.2178 0 0)',
+				bgLight: 'oklch(0.2603 0 0)',
+				textMain: 'oklch(0.9491 0 0)',
+				textMuted: 'oklch(0.6993 0 0)',
+				border: 'oklch(0.4017 0 0)',
+			});
+		}
+
+		if (theme == 'light') {
+			setColorVariables('light', {
+				bgDark: 'oklch(0.8522 0 0)',
+				bgNormal: 'oklch(0.9078 0 0)',
+				bgLight: 'oklch(0.9603 0 0)',
+				textMain: 'oklch(0.1491 0 0)',
+				textMuted: 'oklch(0.2993 0 0)',
+				border: 'oklch(0.4017 0 0)',
+			});
+		}
+	}, [theme]);
+}
+
+function setColorVariables(lightness: 'dark' | 'light', { bgDark, bgNormal, bgLight, textMain, textMuted, border }: Record<string, string>) {
+	document.documentElement.style.setProperty('--bg-dark', bgDark);
+	document.documentElement.style.setProperty('--bg-base', bgNormal);
+	document.documentElement.style.setProperty('--bg-light', bgLight);
+	document.documentElement.style.setProperty('--text-main', textMain);
+	document.documentElement.style.setProperty('--text-muted', textMuted);
+	document.documentElement.style.setProperty('--border', border);
+
+	let accent = lightness == 'dark' ? 'oklch(0.6802 0.1606 278.08)' : 'oklch(0.4296 0.1606 278.08)';
+	let accentMuted = lightness == 'dark' ? 'oklch(0.3296 0.0529 280.15)' : 'oklch(0.8002 0.0529 280.15)';
+
+	document.documentElement.style.setProperty('--accent', accent);
+	document.documentElement.style.setProperty('--accent-muted', accentMuted);
+}
