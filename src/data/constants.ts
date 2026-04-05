6a72b9bcc8a3bcef6758212e9304238c6667fb69
Thrillonek <jindrich.kraina@gmail.com>
2026-04-05 12:08:11 +0200

    The "view current folder" button now switches folders if not in the correct one

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,11 +1,3 @@
-import { create } from 'zustand';
-
-export const useFileSystemStore = create((set) => ({
-	fileSystem: null,
-	setFileSystem: (fileSystem: any) => set({ fileSystem }),
-}));
-
-const isVite = typeof import.meta !== 'undefined' && typeof import.meta.env !== 'undefined';
 //@ts-expect-error
-export const fileSystem = isVite ? __FILE_SYSTEM__ : useFileSystemStore?.getState().fileSystem;
+export const fileSystem = __FILE_SYSTEM__;
 export const allowedFileExtensions = ['jpg', 'jpeg', 'png', 'gif', 'apng', 'avif', 'svg', 'webp'];
