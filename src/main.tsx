b85e663067e7e6e8a2e1f77b8b217b65bb46addd
Karel <karelos101@gmail.com>
2026-05-27 13:47:47 +0200

    Basic impl of NextJS

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'src/assets/tailwindInit.css';
-import { App } from './app/index.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "src/assets/tailwindInit.css";
+import App from "./app/index.jsx";
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 
 ReactDOM.createRoot(root!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
